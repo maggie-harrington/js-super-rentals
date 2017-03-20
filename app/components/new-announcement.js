16da5dff2773bc39ae0be09ea51bf565058a15db
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-20 15:30:21 -0700

    add component behavior of save button for new announcement

--- app/components/new-announcement.js
+++ app/components/new-announcement.js
@@ -5,6 +5,17 @@ export default Ember.Component.extend({
     actions: {
         rentalFormShow() {
             this.set('addNewAnnouncement', true);
+        },
+
+        saveAnnouncement1() {
+            var params = {
+                title: this.get('title'),
+                start: this.get('start') ? this.get('start') : "",
+                end: this.get('end') ? this.get('end') : "",
+                message: this.get('message'),
+            };
+            this.set('addNewAnnouncement', false);
+            this.sendAction('saveAnnouncement2', params)
         }
     }
 });

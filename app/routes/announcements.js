9273c4f1ff7c4dbdd6e3f0d7aa578954004937f4
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-20 14:01:09 -0700

    add destroyAnnouncement action to template and route handler

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('announcement');
+    },
+
+    actions: {
+        destroyAnnouncement(announcement) {
+            announcement.destroyAnnouncement();
+            this.transitionTo('announcements');
+        }
     }
 });

6d8fb16ba912d04f33909c897a7303c3b9d3cea3
Maggie Harrington <maggie.harrington@gmail.com>
2017-03-20 15:10:56 -0700

    add toggle to new announcement form

--- app/components/new-announcement.js
+++ app/components/new-announcement.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    addNewAnnouncement: false,
+    actions: {
+        rentalFormShow() {
+            this.set('addNewAnnouncement', true);
+        }
+    }
 });
